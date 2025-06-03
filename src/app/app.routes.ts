72f55e3a36161f9b21618d3dcfde602a8a11415c
Rajeswaran Dhandapani <rajeswaran.dhandapani@gmail.com>
2025-06-03 14:37:08 -0400

    add welcome route to application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {WelcomeComponent} from './welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+];
