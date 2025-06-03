17096fe263fbd696be5796b2e5a68e15da33c987
Rajeswaran Dhandapani <rajeswaran.dhandapani@gmail.com>
2025-06-03 16:09:05 -0400

    implement login and registration functionality on welcome page

--- src/app/welcome.component.ts
+++ src/app/welcome.component.ts
@@ -10,6 +10,10 @@ import {CommonModule} from '@angular/common';
 })
 export class WelcomeComponent implements OnInit {
 
+  // Keycloak base URL and realm (update as needed)
+  private keycloakBaseUrl = 'http://localhost:8089/realms/baas';
+  private clientId = 'banking-app';
+
   constructor() { }
 
   ngOnInit(): void {
@@ -17,5 +21,13 @@ export class WelcomeComponent implements OnInit {
     // You might need to add Bootstrap icons to your project if not already installed
     // npm install bootstrap-icons
     // And import in your main styles.scss: @import 'bootstrap-icons/font/bootstrap-icons.css';
+  }
+
+  login() {
+    window.location.href = `${this.keycloakBaseUrl}/protocol/openid-connect/auth?client_id=${this.clientId}&response_type=code&scope=openid&redirect_uri=${encodeURIComponent(window.location.origin)}`;
+  }
+
+  register() {
+    window.location.href = `${this.keycloakBaseUrl}/protocol/openid-connect/registrations?client_id=${this.clientId}&response_type=code&scope=openid&redirect_uri=${encodeURIComponent(window.location.origin)}`;
   }
 }
